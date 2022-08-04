64b59745be2dfea05921c8ce5b29c20aa011ad87
Damien Irving <irving.damien@gmail.com>
2022-08-04 14:44:50 +1000

    Two panels

--- plot_seatac_TXx_histogram.py
+++ plot_seatac_TXx_histogram.py
@@ -38,37 +38,47 @@ def _main(args):
     bias_shape, bias_loc, bias_scale = indices.fit_gev(ds_bias_stacked['tasmax'].values, use_estimates=True)
     logging.info(f'Model (bias corrected) GEV fit: shape={bias_shape}, location={bias_loc}, scale={bias_scale}')
 
-    fig, ax = plt.subplots(figsize=[10, 8])
+    fig = plt.figure(figsize=[10, 16])
+    ax1 = fig.add_subplot(211)
+
     bins = np.arange(23, 49)
     gev_xvals = np.arange(22, 49, 0.1)
     
-    ds_bias_stacked['tasmax'].plot.hist(bins=bins,
-                                        density=True,
-                                        rwidth=0.9,
-                                        alpha=0.7,
-                                        color='tab:blue',
-                                        label='ACCESS-D')
+    ds_bias_stacked['tasmax'].plot.hist(
+        ax=ax1,
+        bins=bins,
+        density=True,
+        rwidth=0.9,
+        alpha=0.7,
+        color='tab:blue',
+        label='ACCESS-D'
+    )
     bias_pdf = gev.pdf(gev_xvals, bias_shape, bias_loc, bias_scale)
-    plt.plot(gev_xvals, bias_pdf, color='tab:blue', linewidth=2.0)
+    ax1.plot(gev_xvals, bias_pdf, color='tab:blue', linewidth=2.0)
     raw_pdf = gev.pdf(gev_xvals, raw_shape, raw_loc, raw_scale)
-    plt.plot(gev_xvals, raw_pdf, color='tab:blue', linestyle='--', linewidth=2.0)
+    ax1.plot(gev_xvals, raw_pdf, color='tab:blue', linestyle='--', linewidth=2.0)
 
-    ds_obs['tasmax'].plot.hist(bins=bins,
-                               density=True,
-                               rwidth=0.9,
-                               alpha=0.7,
-                               color='tab:orange',
-                               label='Station Observations')
+    ds_obs['tasmax'].plot.hist(
+        ax=ax1,
+        bins=bins,
+        density=True,
+        rwidth=0.9,
+        alpha=0.7,
+        color='tab:orange',
+        label='Station Observations'
+    )
     obs_pdf = gev.pdf(gev_xvals, obs_shape, obs_loc, obs_scale)
-    plt.plot(gev_xvals, obs_pdf, color='tab:orange', linewidth=2.0)
+    ax1.plot(gev_xvals, obs_pdf, color='tab:orange', linewidth=2.0)
 
-    plt.legend()
-    plt.xlabel('TXx (C)')
-    plt.ylabel('probability')
-    plt.title('Histogram of TXx: SeaTac')
+    ax1.legend()
+    ax1.set_xlabel('TXx (C)')
+    ax1.set_ylabel('probability')
+    ax1.set_title('Histogram of TXx: SeaTac')
 
-    infile_logs = {args.bias_corrected_model_file: ds_bias.attrs['history'],
-                   args.obs_file: ds_obs.attrs['history']}
+    infile_logs = {
+        args.bias_corrected_model_file: ds_bias.attrs['history'],
+        args.obs_file: ds_obs.attrs['history']
+    }
     repo_dir = sys.path[0]
     new_log = fileio.get_new_log(infile_logs=infile_logs, repo_dir=repo_dir)
     metadata_key = fileio.image_metadata_keys[args.outfile.split('.')[-1]]
