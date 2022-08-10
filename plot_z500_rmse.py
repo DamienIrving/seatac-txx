d612f1396af415575f33f56d535acb4c511ad66a
Damien Irving <irving.damien@gmail.com>
2022-08-10 16:08:39 +1000

    Add logging

--- plot_z500_rmse.py
+++ plot_z500_rmse.py
@@ -3,6 +3,7 @@
 import pdb
 import sys
 import argparse
+import logging
 import warnings
 warnings.filterwarnings('ignore')
 
@@ -26,7 +27,9 @@ def find_max_z500(da_tasmax, da_h500_box):
     max_ensemble_idx = int(da_tasmax.argmax(dim=['time', 'ensemble'])['ensemble'].data)
     max_tasmax_from_idx = float(da_tasmax.isel({'time': max_time_idx, 'ensemble': max_ensemble_idx}).values)
     assert max_tasmax == max_tasmax_from_idx
-    print(f'Hottest day of {max_tasmax} on {max_time_date}, ensemble member {max_ensemble_idx + 1}')
+    log_text = f'Hottest day of {max_tasmax} on {max_time_date}, ensemble member {max_ensemble_idx + 1}'
+    logging.info(log_text)
+    print(log_text)
     max_z500 = da_h500_box.isel({'time': max_time_idx, 'ensemble': max_ensemble_idx})
     
     return max_z500
@@ -40,6 +43,8 @@ def _main(args):
     """Run the command line program."""
 
     plotting_utils.set_plot_params(args.plotparams)
+    logfile = args.logfile if args.logfile else args.outfile.split('.')[0] + '.log'
+    logging.basicConfig(level=logging.INFO, filename=logfile, filemode='w')
     
     lat = 47.45
     lon = 237.69
@@ -51,7 +56,8 @@ def _main(args):
     ]
     
     ensemble_max_tasmax = 0
-    for infile in args.infiles:
+    files_to_search = [args.txxmax_file] if args.txxmax_file else args.infiles
+    for infile in files_to_search:
         ds = fileio.open_dataset(
             infile,
             variables=['h500', 'tasmax'],
@@ -116,7 +122,8 @@ def _main(args):
         args.outfile,
         metadata={metadata_key: new_log},
         bbox_inches='tight',
-        facecolor='white'
+        facecolor='white',
+        dpi=300,
     )
 
         
@@ -126,9 +133,29 @@ if __name__ == '__main__':
     parser.add_argument("infiles", type=str, nargs='*', help="Input files")
     parser.add_argument("model_config", type=str, help="model configuration file")
     parser.add_argument("outfile", type=str, help="Output file")
-    parser.add_argument('--plotparams', type=str, default=None,
-                        help='matplotlib parameters (YAML file)')
-    parser.add_argument('--distance', type=float, default=30,
-                        help='distance (in degrees of lat/lon) analysis box should extend from SeaTac airport')
+    parser.add_argument(
+        '--plotparams',
+        type=str,
+        default=None,
+        help='matplotlib parameters (YAML file)'
+    )
+    parser.add_argument(
+        '--distance',
+        type=float,
+        default=30,
+        help='distance (in degrees of lat/lon) analysis box should extend from SeaTac airport'
+    )
+    parser.add_argument(
+        '--logfile',
+        type=str,
+        default=None,
+        help='name of logfile (default = same as outfile but with .log extension'
+    )
+    parser.add_argument(
+        '--txxmax_file',
+        type=str,
+        default=None,
+        help='file containing the hottest day'
+    )
     args = parser.parse_args()
     _main(args)
